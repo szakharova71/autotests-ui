115cc31d1d6a134b1cc73686647b76d6a08f9f02
szakharova71 <szakharova86@gmail.com>
2026-01-14 16:22:46 +0300

    Введение в Playwright.Практика с Playwright

---
+++ playwright_registration.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    # Переходим на страницу регистрации
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    # Заполняем поле email
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    # Заполняем поле username
+    email_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    email_input.fill("username")
+
+    # Заполняем поле пароль
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+
+    # Нажимаем на кнопку Registration
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # Проверяем, что отображается заголовок Dashboard
+    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_header).to_be_visible()
+    expect(dashboard_header).to_have_text("Dashboard")
+
+    # Задержка для наглядности выполнения теста (не рекомендуется использовать в реальных автотестах)
+    page.wait_for_timeout(5000)
