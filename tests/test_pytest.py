c61e242496c430e70c003194ca290cf37972e57e
szakharova71 <szakharova86@gmail.com>
2026-01-19 15:24:02 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_user_login():
+    print("Hello!")
+
+class TestUserLogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+def test_assert_positive_case():  # Новый тест, которые проверяет положительный кейс
+    assert (2 + 2) == 4  # Ожидается, что тест пройдет
+
+
+def test_assert_negative_case():  # Новый тест, которые проверяет негативный кейс
+    assert (2 + 2) == 5  # Тут должна быть ошибка
