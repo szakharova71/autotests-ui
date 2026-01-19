4f60f333175ee5e85451953fb8ab2ba474b6bdbd
szakharova71 <szakharova86@gmail.com>
2026-01-19 15:29:12 +0300

    Интеграция Pytest и Playwright

---
+++ tests/test_registration.py
@@ -0,0 +1,45 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_successful_registration():  # Создаем тестовую функцию
+    with sync_playwright() as playwright:
+        # Запускаем Chromium браузер в обычном режиме (не headless)
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()  # Создание контекста
+        page = context.new_page()  # Создание страницы
+
+        # Переходим на страницу регистрации
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        # Заполняем поле email
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill("user.name@gmail.com")
+
+        # Заполняем поле username
+        email_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        email_input.fill("username")
+
+        # Заполняем поле пароль
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill("password")
+
+        # Нажимаем на кнопку Registration
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        # Проверяем, что отображается заголовок Dashboard
+        dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
+        expect(dashboard_header).to_be_visible()
+        expect(dashboard_header).to_have_text("Dashboard")
+
+        # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
+        context.storage_state(path="browser-state.json")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+        page.wait_for_timeout(5000)
