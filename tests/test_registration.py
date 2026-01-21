1f2ce737b2ba35ccebf28f66f575ed2d745ecb29
szakharova71 <szakharova86@gmail.com>
2026-01-21 23:53:33 +0300

    Знакомство с PageObject.Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,30 +1,14 @@
-from playwright.sync_api import  expect, Page
 import pytest
+
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
+
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):  # Создаем тестовую функцию
-    # Переходим на страницу регистрации
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(email="user.name@gmail.com", username="username", password="password")
+    registration_page.click_registration_button()
 
-    # Заполняем поле email
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
-
-    # Заполняем поле username
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill("username")
-
-    # Заполняем поле пароль
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
-
-    # Нажимаем на кнопку Registration
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    # Проверяем, что отображается заголовок Dashboard
-    dashboard_header = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_be_visible()
-    expect(dashboard_header).to_have_text("Dashboard")
-
+    dashboard_page.check_dashboard_title()
