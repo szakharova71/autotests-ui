a46d680207c24a7579976eedad1aebea41eac8ea
szakharova71 <szakharova86@gmail.com>
2026-01-26 15:53:39 +0300

    Практическое применение PageFactory в автотестах

--- elements/base_element.py
+++ elements/base_element.py
@@ -8,24 +8,28 @@ class BaseElement:
         self.locator = locator
 
     # Метод принимает кейворд аргументы (kwargs)
-    def get_locator(self, **kwargs) -> Locator: # объект Locator для взаимодействия с элементом
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator: # объект Locator для взаимодействия с элементом
         # Инициализирует объект локатора, подставляя динамические значения в локатор.
+        # Добавляем аргумент nth со значением по умолчанию 0
         locator = self.locator.format(**kwargs)
         # Возвращаем объект локатора
-        return self.page.get_by_test_id(locator)
+        return self.page.get_by_test_id(locator).nth(nth)  # Теперь выбираем элемент по индексу
 
-    def click(self, **kwargs):
+    def click(self, nth: int = 0, **kwargs):
         # "Лениво" инициализируем локатор
-        locator = self.get_locator(**kwargs)
+        # Добавили аргумент nth и передаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         # Выполняем нажатие на элемент
         locator.click()
 
-    def check_visible(self, **kwargs):
+    def check_visible(self, nth: int = 0, **kwargs):
         # "Лениво" инициализируем локатор
-        locator = self.get_locator(**kwargs)
+        # Добавили аргумент nth и передаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         # Проверяем, что элемент виден на странице
         expect(locator).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_text(text)
