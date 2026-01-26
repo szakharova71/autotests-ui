fb7270361a97324654eff3677a0b04f4395ea782
szakharova71 <szakharova86@gmail.com>
2026-01-26 14:09:11 +0300

    Знакомство с PageFactory

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import Page, expect
 
 from components.authentication.login_form_component import LoginFormComponent
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
 from pages.base_page import BasePage
 
 
@@ -10,9 +13,11 @@ class LoginPage(BasePage):
 
         self.login_form=LoginFormComponent(page)
 
-        self.login_button = page.get_by_test_id('login-page-login-button')
-        self.registration_link = page.get_by_test_id('login-page-registration-link')
-        self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
+        self.login_button = Button(page, 'login-page-login-button', 'Login')
+        self.registration_link = Link(page, 'login-page-registration-link', 'Registration')
+        self.wrong_email_or_password_alert = Text(
+            page, 'login-page-wrong-email-or-password-alert', 'Wrong email or password'
+        )
 
     # Метод для нажатия на кнопку "Login"
     def click_login_button(self):
@@ -24,5 +29,5 @@ class LoginPage(BasePage):
 
     # Метод для проверки отображения алерта с ошибкой
     def check_visible_wrong_email_or_password_alert(self):
-        expect(self.wrong_email_or_password_alert).to_be_visible()
-        expect(self.wrong_email_or_password_alert).to_have_text("Wrong email or password")
+        self.wrong_email_or_password_alert.check_visible()
+        self.wrong_email_or_password_alert.check_have_text('Wrong email or password')
