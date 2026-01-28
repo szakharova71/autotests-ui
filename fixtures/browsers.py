a6369eb38d4197b850593292fb8a6df1c28e57a3
szakharova71 <szakharova86@gmail.com>
2026-01-28 16:22:28 +0300

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -5,6 +5,7 @@ from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
+from config import settings  # Импортируем настройки
 
 
 @pytest.fixture
@@ -14,18 +15,20 @@ def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-    # Запускаем Chromium браузер в обычном режиме (не headless)
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()  # Создание контекста
-    page = context.new_page()  # Создание страницы
+    browser = playwright.chromium.launch(headless=settings.headless)  # Используем settings.headless
+    context = browser.new_context()
+    page = context.new_page()
 
-    # Работаем с регистрационной страницей через Page Object
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,  # Используем settings.test_user.email
+        username=settings.test_user.username,  # Используем settings.test_user.username
+        password=settings.test_user.password  # Используем settings.test_user.password
+    )
     registration_page.click_registration_button()
 
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path=settings.browser_state_file)  # Используем settings.browser_state_file
     browser.close()
 
 
@@ -34,5 +37,5 @@ def chromium_page_with_state(initialize_browser_state, request: SubRequest, play
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state="browser-state.json"
+        storage_state=settings.browser_state_file  # Используем settings.browser_state_file
     )
--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,21 +1,26 @@
 import allure
 from playwright.sync_api import Playwright, Page
 
+from config import settings  # Импортируем настройки
 
 def initialize_playwright_page(
         playwright: Playwright,
         test_name: str,
         storage_state: str | None = None
 ) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state=storage_state, record_video_dir='./videos')
+    # Используем settings.headless
+    browser = playwright.chromium.launch(headless=settings.headless)
+    # Используем settings.videos_dir
+    context = browser.new_context(storage_state=storage_state, record_video_dir=settings.videos_dir)
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
 
     yield page
 
-    context.tracing.stop(path=f'./tracing/{test_name}.zip')
+    # Используем settings.tracing_dir
+    context.tracing.stop(path=settings.tracing_dir.joinpath(f'{test_name}.zip'))
     browser.close()
 
-    allure.attach.file(f'./tracing/{test_name}.zip', name='trace', extension='zip')
+    # Используем settings.tracing_dir
+    allure.attach.file(settings.tracing_dir.joinpath(f'{test_name}.zip'), name='trace', extension='zip')
     allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
