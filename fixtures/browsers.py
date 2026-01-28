95fb803f3aa8ebf7f6b1cd82f27b10f05e1dd85e
szakharova71 <szakharova86@gmail.com>
2026-01-28 12:45:59 +0300

    Добавление Playwright Trace Viewer в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,19 +1,28 @@
+import allure
 import pytest  # Импортируем pytest
-from playwright.sync_api import Playwright, \
-    Page  # Импортируем класс страницы, будем использовать его для аннотации типов
+from _pytest.fixtures import SubRequest  # Импортируем класс SubRequest для аннотации
+from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
 
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
+def chromium_page(request: SubRequest, playwright: Playwright) -> Page:  # Добавили аргумент request
     # Ниже идет инициализация и открытие новой страницы
-    # Запускаем браузер
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False) # Запускаем браузер
+    context = browser.new_context()  # Создаем контекст для новой сессии браузера
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)  # Включаем трейсинг
+
     # Передаем страницу для использования в тесте
-    yield browser.new_page()
+    yield context.new_page()
+
+    # В данном случае request.node.name содержит название текущего автотеста
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')  # Сохраняем трейсинг в файл
     # Закрываем браузер после выполнения тестов
     browser.close()
+
+    # Прикрепляем файл с трейсингом к Allure отчету
+    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
 
 
 @pytest.fixture(scope="session")
@@ -34,8 +43,15 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)  # Включаем трейсинг
+
     yield context.new_page()
+
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')  # Сохраняем трейсинг в файл
     browser.close()
+
+    # Прикрепляем файл с трейсингом к Allure отчету
+    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
