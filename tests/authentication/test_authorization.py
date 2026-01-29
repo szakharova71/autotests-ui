60054c473f26f606480defbe5f15ec9b7bfde3a6
szakharova71 <szakharova86@gmail.com>
2026-01-29 13:09:07 +0300

    Параллельный запуск автотестов

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -23,6 +23,7 @@ from tools.routes import AppRoute
 @allure.suite(AllureFeature.AUTHENTICATION)
 @allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+    @pytest.mark.xdist_group(name="authorization-group")  # Добавили xdist группу
     @pytest.mark.parametrize("email, password",
                              [("user.name@gmail.com", "password"), ("user.name@gmail.com", "  "), ("  ", "password")])
     @allure.tag(AllureTag.USER_LOGIN)  # Используем enum
