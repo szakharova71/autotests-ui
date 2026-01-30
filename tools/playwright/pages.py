30e4bd6ac586f4c13868ea059c153a692ed1a560
szakharova71 <szakharova86@gmail.com>
2026-01-30 15:17:05 +0300

    Моки и оптимизация UI автотестов

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,6 +2,7 @@ import allure
 from playwright.sync_api import Playwright, Page
 
 from config import settings, Browser  # Импортируем enum Browser
+from tools.playwright.mocks import mock_static_resources  # Импортируем функцию mock_static_resources
 
 
 
@@ -21,6 +22,7 @@ def initialize_playwright_page(
                                   )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)  # Отключаем загрузку статических ресурсов
 
     yield page
 
